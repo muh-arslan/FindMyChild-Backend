49fcca69990c923cd74fbdde837699c785f76cf3
Muhammad Arslan <108022309+muh-arslan@users.noreply.github.com>
2023-05-17 14:55:50 +0500

    Message Updated

--- login_app/email.py
+++ login_app/email.py
@@ -3,9 +3,8 @@ from django.conf import settings
 
 
 def send_otp_via_email(email, otp):
-    print('email sent')
     subject = "Email Verification"
-    message = f'Your OTP For FindMyChild is {otp}'
+    message = f'Your One-Time Password (OTP) for verification is: {otp}. Please use this code to verify your email.'
     email_from = settings.EMAIL_HOST
     try:
         send_mail(subject, message, email_from, [email])
