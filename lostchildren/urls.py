27da9a6a87bc8b57c6d340b157712ee9561a1928
MUJEEB-UR-REHMAN54 <75685931+MUJEEB-UR-REHMAN54@users.noreply.github.com>
2023-07-02 23:26:49 +0500

    get image url updates

--- lostchildren/urls.py
+++ lostchildren/urls.py
@@ -19,10 +19,7 @@ urlpatterns = [
          name='received_child_view'),
     path('change-status/', UpdateChildStatus.as_view(),
          name='update_child_status_view'),
-    path('image/found/<uuid:child_id>/', image_view,
-         {'child_model': 'found'}, name='found_child_image_view'),
-    path('image/lost/<uuid:child_id>/', image_view,
-         {'child_model': 'lost'}, name='lost_child_image_view'),
+    path('image/child/<uuid:child_id>/', image_view, name='found_child_image_view'),
     path('report/<uuid:pk>/', ReportUpdateAPIView.as_view(), name='lost_child_update_view'),
     path('create-received-report/', ReceivedChildCreate.as_view(), name='received_child_create_view'),
 
